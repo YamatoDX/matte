021056f6589d7d8597fc31cb311d0f41d59ec1d5
YamatoDX <piyal@twurs.com>
2021-08-25 01:04:09 +0600

    robot font added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React,{useState} from "react";
-import {BrowserRouter as Router} from "react-router-dom"
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-export default function App(){
-  const arr = ["apple","orange"];
-  return(
+export default function App() {
+  const arr = ["apple", "orange"];
+  return (
     <>
-    {arr.map(each => (
-      <div key={each}>
-        {each}
-      </div>
-    ))}
+      {arr.map((each) => (
+        <div 
+        key={each}>{each}</div>
+      ))}
     </>
-  )
+  );
 }
