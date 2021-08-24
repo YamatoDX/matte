7129e0f24ce83b0a20ae7b571cac222f95da1ef5
YamatoDX <piyal@twurs.com>
2021-08-25 02:41:53 +0600

    some changes in the files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { createTheme, ThemeProvider } from "@material-ui/core";
+import { createTheme, ThemeProvider, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import Button from "@material-ui/core/Button";
 import Switch from "@material-ui/core/Switch";
 import {
@@ -8,12 +9,18 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
-  CheckBox,
+  Checkbox,
   Typography,
   TextField,
 } from "@material-ui/core";
+const useStyles = makeStyles({
+  labelStyles: {
+    marginBottom: "20",
+  },
+});
 
 export default function App() {
+  const classes = useStyles();
   const defaultState = {
     isDark: false,
     currentName: "",
@@ -29,74 +36,80 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Switch
-          checked={state.isDark}
-          color="primary"
-          onChange={() =>
-            setState({
-              ...state,
-              isDark: !state.isDark,
-            })
-          }
-        />
-        <Typography>Click the above switch to change theme</Typography>
-        {/*  */}
-        <FormControl variant="outlined">
-          <FormLabel>This is the first label people</FormLabel>
-          <TextField
-            type="text"
-            onChange={(e) =>
+        <Container>
+          <Switch
+            checked={state.isDark}
+            color="primary"
+            onChange={() =>
               setState({
                 ...state,
-                currentName: e.target.value,
+                isDark: !state.isDark,
               })
             }
-            label="Current Name"
-            required={true}
-            helperText="Fill out with your current Name"
-            variant="outlined"
           />
+          <Typography>Click the above switch to change theme</Typography>
           {/*  */}
-          <RadioGroup
-            onChange={(e) =>
-              setState({ ...state, currentGender: e.target.value })
-            }
-          >
-            <FormLabel>Select your gender: </FormLabel>
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="male"
-              label="Male"
+          <FormControl variant="outlined">
+            <FormLabel className={classes.labelStyles}>
+              This is the first label people
+            </FormLabel>
+            <TextField
+              type="text"
+              onChange={(e) =>
+                setState({
+                  ...state,
+                  currentName: e.target.value,
+                })
+              }
+              label="Current Name"
+              required={true}
+              helperText="Fill out with your current Name"
+              variant="outlined"
             />
+            {/*  */}
+            <RadioGroup
+              onChange={(e) =>
+                setState({ ...state, currentGender: e.target.value })
+              }
+            >
+              <FormLabel>Select your gender: </FormLabel>
+              <FormControlLabel
+                control={<Radio color="primary" />}
+                value="male"
+                label="Male"
+              />
+              <FormControlLabel
+                control={<Radio color="primary" />}
+                value="female"
+                label="female"
+              />
+              <FormControlLabel
+                control={<Radio color="primary" />}
+                value="others"
+                label="others"
+              />
+            </RadioGroup>
+            {/*  */}
+            <FormLabel>
+              Select if you think you are much alike goku himself
+            </FormLabel>
             <FormControlLabel
-              control={<Radio color="primary" />}
-              value="female"
-              label="female"
+              control={<Checkbox color="secondary" />}
+              value={state.isGoku}
+              label="Tick if you are goku"
             />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="others"
-              label="others"
-            />
-          </RadioGroup>
-          {/*  */}
-          <FormLabel>
-            Select if you think you are much alike goku himself
-          </FormLabel>
-          <FormControlLabel
-            control={<CheckBox color="secondary" />}
-            value={state.isGoku}
-            label="Tick if you are goku"
-          />
-          <Button
-            onClick={() => {
-              console.log(state);
-              setState(defaultState);
-            }}
-          >
-            SUBMIT
-          </Button>
-        </FormControl>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={() => {
+                console.log(state);
+                setState(defaultState);
+              }}
+            >
+              SUBMIT
+            </Button>
+          </FormControl>
+        </Container>
       </ThemeProvider>
     </>
   );
